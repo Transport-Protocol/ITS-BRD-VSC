e22118272a709ab147790480a7f0930d92a8fd00
FranzKorf <frakorf@gmail.com>
2026-08-15 19:38:58 +0200

    clang_tidy_result.txt aus Konfigurationsdateien entfernt

--- bin/createGenericProject.ps1
+++ bin/createGenericProject.ps1
@@ -161,7 +161,6 @@ function Main {
     # Erstellte .vscode.d Verzeichnis - nicht schoen, aber einfach und robust
     mkdir "$project_dir\.vscode.d"
     cp "$template_dir\.vscode.d\tasks.json"  "$project_dir\.vscode.d"
-    cp "$template_dir\clang_tidy_result.txt" "$project_dir"
 
     Write-Host "Projekt '$project_name' erfolgreich erstellt in '$project_dir'."
 
