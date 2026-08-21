6b4d1f7aac73ab7196f6f9ad0b247c9de4ceab3e
FranzKorf <frakorf@gmail.com>
2026-08-21 06:15:55 +0200

    c-template mit lint bearbeitet

--- bin/template/c-project/Src/main.c
+++ bin/template/c-project/Src/main.c
@@ -1,35 +1,32 @@
 /**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Franz Korf
-  * @brief   Kleines Testprogramm fuer neu erstelle Fonts.
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    main.c
+ * @author  Franz Korf
+ * @brief   Kleines Testprogramm fuer neu erstelle Fonts.
+ ******************************************************************************
+ */
 /* Includes ------------------------------------------------------------------*/
 
-#include "stm32f4xx_hal.h"
-#include "init.h"
 #include "LCD_GUI.h"
 #include "LCD_Touch.h"
+#include "init.h"
 #include "lcd.h"
-#include "fontsFLASH.h"
-#include "additionalFonts.h"
-#include "error.h"
-
+#include "stm32f4xx_hal.h"
 
 int main(void) {
-	initITSboard();    // Initialisierung des ITS Boards
-	
-	GUI_init(DEFAULT_BRIGHTNESS);   // Initialisierung des LCD Boards mit Touch
-	TP_Init(false);                 // Initialisierung des LCD Boards mit Touch
+   initITSboard(); // Initialisierung des ITS Boards
 
-  // Begruessungstext	
-	lcdPrintlnS("Hallo liebes TI-Labor (c-project)");
-	
-	// Test in Endlosschleife
-	while(1) {
-		HAL_Delay(10000);
-	}
+   GUI_init(DEFAULT_BRIGHTNESS); // Initialisierung des LCD Boards mit Touch
+   TP_Init(false); // Initialisierung des LCD Boards mit Touch
+
+   // Begruessungstext
+   lcdPrintlnS("Hallo liebes TI-Labor (c-project)");
+
+   // Test in Endlosschleife
+   const int POLL_DELAY = 10000;
+   while (1) {
+      HAL_Delay(POLL_DELAY);
+   }
 }
 
 // EOF
